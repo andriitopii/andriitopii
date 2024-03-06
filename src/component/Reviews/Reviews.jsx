a78c39988f89d53e3addb10ee74c4a7784cc083b
Andrii Topii <andriitopii@icloud.com>
2024-03-06 21:38:22 +0100

    asdasd

--- src/component/Reviews/Reviews.jsx
+++ src/component/Reviews/Reviews.jsx
@@ -1,9 +1,130 @@
+import { useForm } from "react-hook-form";
+import "./Reviews.scss";
+import { useEffect, useState } from "react";
+import { app } from "../../bd/firebase";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  arrayUnion,
+  addDoc,
+  collection,
+  getDocs,
+} from "firebase/firestore";
 const Reviews = () => {
-    return ( <section id="reviews">
-    <div className="container container--col container--background-dark">
-    <h1 className="title-section title-section--light">Reviews</h1>
-    </div>
-</section> );
-}
- 
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+    getValues,
+  } = useForm({ mode: "onChange" });
+
+  const [allOpinion, setAllOpinion] = useState([]);
+  const db = getFirestore(app);
+  const sendForm = async (data) => {
+    const username = getValues("username");
+    const useremail = getValues("useremail");
+    const useropinion = getValues("useropinion");
+    const docRef = await addDoc(collection(db, "reviews"), {
+      name: username,
+      email: useremail,
+      opinion: useropinion,
+    });
+    setAllOpinion([{ name: username, opinion: useropinion }, ...allOpinion ]);
+
+    reset();
+  };
+
+  useEffect(() => {
+    async function getReviews() {
+      const querySnapshot = await getDocs(collection(db, "reviews"));
+      const arrData = [];
+
+      querySnapshot.forEach((doc) => {
+        arrData.push(doc.data());
+      });
+      setAllOpinion(arrData);
+    }
+    getReviews();
+  }, []);
+
+  return (
+    <section id="reviews">
+      <div className="container container--col container--background-dark">
+        <h1 className="title-section title-section--light">Reviews</h1>
+        <div className="reviews">
+          <div className="reviews__form">
+            <form onSubmit={handleSubmit(sendForm)}>
+              <input
+                type="text"
+                placeholder="Name"
+                {...register("username", {
+                  required: "Поле обовязкове для заповнення",
+                  minLength: { value: 1, message: "Мінімум 1 символів" },
+                  pattern: {
+                    value: /^[^\d\W]*$/,
+                    message: "Не припустимі символи",
+                  },
+                })}
+              />
+              <div className="input__error">
+                {errors?.username && (
+                  <p>{errors?.username?.message || "Error"}</p>
+                )}
+              </div>
+              <input
+                type="email"
+                placeholder="Email"
+                {...register("useremail", {
+                  required: "Поле обовязкове для заповнення",
+                  minLength: { value: 5, message: "Мінімум  символів" },
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Не коректний Email",
+                  },
+                })}
+              />
+              <div className="input__error">
+                {errors?.useremail && (
+                  <p>{errors?.useremail?.message || "Error"}</p>
+                )}
+              </div>
+              <textarea
+                placeholder="Your opinion"
+                cols="30"
+                rows="5"
+                {...register("useropinion", {
+                  required: "Поле обовязкове для заповнення",
+                  minLength: { value: 5, message: "Мінімум 5 символів" },
+                  maxLength: { value: 100, message: "Максимум 100 символів" },
+                })}
+              ></textarea>
+              <div className="input__error">
+                {errors?.useropinion && (
+                  <p>{errors?.useropinion?.message || "Error"}</p>
+                )}
+              </div>
+              <button className="btn-form" type="submit" disabled={!isValid}>
+                Submit
+              </button>
+            </form>
+          </div>
+          <div className="reviews__content">
+            {allOpinion.map((item, index) => (
+              <article
+                className="reviews__article"
+                key={index + "article-reviews"}
+              >
+                <h1>{item.name}</h1>
+                <p>{item.opinion}</p>
+              </article>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Reviews;
