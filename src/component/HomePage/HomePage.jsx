055d6f014d625a0abb7f179f87224159bf0438e7
Andrii Topii <andriitopii@icloud.com>
2024-03-07 15:07:21 +0100

    Sdfsdf

--- src/component/HomePage/HomePage.jsx
+++ src/component/HomePage/HomePage.jsx
@@ -7,10 +7,46 @@ import Portfolio from "../Portfolio/Portfolio";
 import Reviews from "../Reviews/Reviews";
 import Contact from "../Contact/Contact";
 import Header from "../Header/Header";
+import { Helmet } from "react-helmet";
 
 const HomePage = () => {
   return (
     <>
+    <Helmet>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <title>Andrii Topii - Website development from A to Z</title>
+    <meta name="description" content="Опис вашого сайту, який буде відображатися у результатах пошуку."/>
+    <meta name="keywords" content="Web development, Website development, Turnkey solutions,Website design
+,Programming
+,UX/UI
+,SEO optimization
+,Mobile development
+,Effective web presence
+,Client satisfaction
+,IT innovations
+,Online visibility
+,Technical support
+,Website performance
+,Customized approach
+,Web development technologies
+,Modern design
+,Rapid development
+,Personalized web experience
+,Interactive website"/>
+    <meta name="author" content="Andrii Topii"/>
+
+ 
+    <meta name="robots" content="index, follow"/>
+    <meta name="googlebot" content="index, follow"/>
+
+    <link rel="canonical" href="Канонічний URL вашої сторінки"/>
+    <link rel="alternate" hreflang="en" href="URL вашої сторінки для англомовного контенту"/>
+
+    </Helmet>
       <Header />
       <AboutUs />
       <Services />
