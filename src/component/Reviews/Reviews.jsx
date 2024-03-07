c61eb388f9e9010f354c27b9ba2581be1e8966ed
Andrii Topii <andriitopii@icloud.com>
2024-03-07 14:31:35 +0100

    sdasdsd

--- src/component/Reviews/Reviews.jsx
+++ src/component/Reviews/Reviews.jsx
@@ -22,6 +22,67 @@ const Reviews = () => {
 
   const [allOpinion, setAllOpinion] = useState([]);
   const db = getFirestore(app);
+
+  //ДАТА ФУНКЦІЯ
+  function getDate (){
+    const date = new Date();
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const mounth = date.getMonth()
+    switch(mounth){
+      case 0:{
+        return (day + " " + "JAN" + " " + year)
+      }
+      break;
+      case 1:{
+        return (day + " " + "FEB" + " " + year)
+      }
+      break;
+      case 2:{
+        return (day + " " + "MAR" + " " + year)
+      }
+      break;
+      case 3:{
+        return (day + " " + "APR" + " " + year)
+      }
+      break;
+      case 4:{
+        return (day + " " + "MAY" + " " + year)
+      }
+      break;
+      case 5:{
+        return (day + " " + "JUN" + " " + year)
+      }
+      break;
+      case 6:{
+        return (day + " " + "JUL" + " " + year)
+      }
+      break;
+      case 7:{
+        return (day + " " + "AUG" + " " + year)
+      }
+      break;
+      case 8:{
+        return (day + " " + "SEP" + " " + year)
+      }
+      break;
+      case 9:{
+        return (day + " " + "OCT" + " " + year)
+      }
+      break;
+      case 10:{
+        return (day + " " + "NOV" + " " + year)
+      }
+      break;
+      case 11:{
+        return (day + " " + "DEC" + " " + year)
+      }
+      break;
+    }
+  }
+  
+
+// ВІДПРАВКА ФОРМИ
   const sendForm = async (data) => {
     const username = getValues("username");
     const useremail = getValues("useremail");
@@ -30,9 +91,12 @@ const Reviews = () => {
       name: username,
       email: useremail,
       opinion: useropinion,
+      date: getDate(),
     });
-    setAllOpinion([{ name: username, opinion: useropinion }, ...allOpinion ]);
-
+    if(allOpinion.length === 6){
+      allOpinion.pop();
+    }
+    setAllOpinion([{ name: username, opinion: useropinion, date: getDate() }, ...allOpinion]);
     reset();
   };
 
@@ -42,9 +106,13 @@ const Reviews = () => {
       const arrData = [];
 
       querySnapshot.forEach((doc) => {
+        if(arrData.length <= 5){
         arrData.push(doc.data());
+        } 
+        setAllOpinion(arrData);
       });
-      setAllOpinion(arrData);
+      
+      console.log(allOpinion);
     }
     getReviews();
   }, []);
@@ -118,6 +186,7 @@ const Reviews = () => {
               >
                 <h1>{item.name}</h1>
                 <p>{item.opinion}</p>
+                <span>{item.date}</span>
               </article>
             ))}
           </div>
