390dee3f3dbcbe88cce3ffce81c60d7ba0a8639f
Andrii Topii <andriitopii@icloud.com>
2024-04-02 13:24:26 +0200

    asdasd

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -4,8 +4,38 @@ import BlogCard from "../BlogCard/BlogCard";
 import { useEffect, useRef, useState } from "react";
 import TitleSection from "../TitleSection/TitleSection";
 import Button from "../Button/Button";
+import ArrowBackSvg from "../Icon/ArrowBackSvg";
+import ArrowForwardSvg from "../Icon/ArrowForwardSvg";
 
 const Blog = () => {
+  const refBlogSlide = useRef(null)
+  function blogSlide(action){
+
+    let scrollLeft = refBlogSlide.current.scrollLeft;
+    const clientWidth = refBlogSlide.current.clientWidth;
+    const scrollWidth = refBlogSlide.current.scrollWidth;
+    const maxScroll = scrollWidth - clientWidth;
+
+    switch(action){
+      case "NEXT":{
+        if((maxScroll - scrollLeft) <= 2){
+          refBlogSlide.current.scrollLeft = 0;
+        }else{
+          refBlogSlide.current.scrollLeft = scrollLeft + (scrollWidth / 3)
+        }
+
+      }
+      break;
+      case "PREV":{
+        if(scrollLeft === 0 || scrollLeft <= 10){
+          refBlogSlide.current.scrollLeft = maxScroll;
+        } else {
+          refBlogSlide.current.scrollLeft = scrollLeft - (scrollWidth / 3);
+        }
+      }
+      break;
+    }
+  }
   return (
     <>
       <section id="blog">
@@ -21,9 +51,13 @@ const Blog = () => {
             </div>
 
             <div className="blog__media">
+              <button onClick={()=>blogSlide("PREV")}><ArrowBackSvg/></button>
+              <div className="blog__media_container" ref={refBlogSlide}>
               <BlogCard />
               <BlogCard />
               <BlogCard />
+              </div>
+              <button onClick={()=>blogSlide("NEXT")}><ArrowForwardSvg/></button>
             </div>
           </div>
         </div>
